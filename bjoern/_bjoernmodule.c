e1ebd7b71abb62ce5268f698b883858bbcedde60
Dani Gonzalez <dani@robbie.ai>
2019-05-24 23:30:12 +0200

    releasing version 4.0.2

--- bjoern/_bjoernmodule.c
+++ bjoern/_bjoernmodule.c
@@ -131,6 +131,6 @@ PyMODINIT_FUNC INIT_BJOERN(void) {
         return NULL;
     }
 
-    PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 4, 0, 1));
+    PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 4, 0, 2));
     return bjoern_module;
 }
